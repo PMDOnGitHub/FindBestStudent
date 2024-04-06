58560632115d81e083612ab67f8b7c9452cbde30
PMD <48282523+PMDOnGitHub@users.noreply.github.com>
2024-04-06 19:51:48 +0200

    Create getName.py

---
+++ getName.py
@@ -0,0 +1,4 @@
+# Make it so this function asks the user for a name, but only accepts names that start with a capital letter and have only letters. Also names should be shorter than 20 letters.
+
+def getName()->Str:
+  pass
